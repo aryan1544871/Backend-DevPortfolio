8f3f52e8246fdd4e7897028e82d96c233612d436
Aryan <ak1544871@gmail.com>
2025-10-22 08:16:32 +0530

    added token feature for retrieving the messages

---
+++ middlewares/identification.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+exports.identifier = (req, res, next) => {
+    let token;
+
+    if (req.headers.client === 'not-browser'){
+        token = req.headers.authorization;
+    }
+    else{
+        token = req.cookies['Authorization'];
+    }
+    if (!token) {
+        return res.status(403).json({ success: false, message: 'Unauthorized' });
+    }
+    try{
+    const userToken =  token.split(' ')[1];
+    const decoded = jwt.verify(userToken, process.env.JWT_SECRET);  
+    if (decoded){
+        req.user = decoded;
+        next();
+    }
+    }
+    catch{
+        throw new Error('error in the token');
+    }
+  };
+
--- routes/messages.js
+++ routes/messages.js
@@ -1,8 +1,9 @@
 const express = require ('express');
 const messagesController = require('../controllers/messages');
+const { identifier } = require('../middlewares/identification');
 const router = express.Router();
 
-router.get('/messages',messagesController.getMessages);
+router.get('/messages',identifier,messagesController.getMessages);
 router.post('/message',messagesController.createMessage);
     
 
