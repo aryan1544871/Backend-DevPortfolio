e59c27ca7b757a7e0fcd6a19a398b5cc790f1c8a
Aryan <ak1544871@gmail.com>
2025-11-27 18:22:13 +0530

    added email functionality for new messages

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Data = require('../models/messages');
+const axios = require('axios');
 
 exports.getMessages = (req,res,next) =>{
     Data.find()
@@ -26,8 +27,27 @@ exports.createMessage = (req,res,next) =>{
     .then (result => 
         {res.status(201).json({
             msg : 'Message created successfully',
-        })})
+        })}
+    )
     .catch ((err)=>{
         console.log(err);
     })
+    const sendData = {
+            email: 'aryan.mnitj@gmail.com',
+            subject: 'New message from your porfolio website',
+            body: `You have received a new message from ${name} (${email}):\n\n${message}`
+          };
+
+          axios.patch(`https://auth-project-swart.vercel.app/api/email/send`, sendData, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(response => {
+              console.log('Email sent:', response.data);
+             
+            })
+            .catch(error => {
+              console.error('Error sending email:', error);
+            });
 }
